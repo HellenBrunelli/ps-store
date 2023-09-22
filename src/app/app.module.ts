b3464ba2b956d89b4200d6937038d4b85f30af9d
Hellen Brunelli <hellenbrunelli01@gmail.com>
2023-09-22 18:13:29 -0300

    [MODULE] Import components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+
+import { CardComponent } from './components/card/card.component';
+import { CardImageComponent } from './components/card/card-image/card-image.component';
+import { CardLabelComponent } from './components/card/card-label/card-label.component';
+import { CardPriceComponent } from './components/card/card-price/card-price.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +19,11 @@ import { FooterComponent } from './components/footer/footer.component';
     HomeComponent,
     HeaderComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    CardComponent,
+    CardImageComponent,
+    CardLabelComponent,
+    CardPriceComponent
   ],
   imports: [
     BrowserModule,
