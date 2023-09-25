0ded80718fdd0ef8a54a87e637cdd57fec275edc
Hellen Brunelli <hellenbrunelli01@gmail.com>
2023-09-25 17:36:45 -0300

    [Modules] New modules added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+
+// Pages
 import { HomeComponent } from './home/home.component';
+
+// Shared
 import { HeaderComponent } from './components/header/header.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 
+// Components
 import { CardComponent } from './components/card/card.component';
 import { CardImageComponent } from './components/card/card-image/card-image.component';
 import { CardLabelComponent } from './components/card/card-label/card-label.component';
 import { CardPriceComponent } from './components/card/card-price/card-price.component';
+
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavSocialComponent } from './components/navbar/nav-social/nav-social.component';
+import { NavMenuComponent } from './components/navbar/nav-menu/nav-menu.component';
+import { NavLogoComponent } from './components/navbar/nav-logo/nav-logo.component';
 
 @NgModule({
   declarations: [
@@ -23,11 +34,16 @@ import { CardPriceComponent } from './components/card/card-price/card-price.comp
     CardComponent,
     CardImageComponent,
     CardLabelComponent,
-    CardPriceComponent
+    CardPriceComponent,
+    NavSocialComponent,
+    NavMenuComponent,
+    NavLogoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
